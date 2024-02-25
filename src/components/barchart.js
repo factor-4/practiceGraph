a11f3dd3f3ad79c7611c1d8f5f2a033fcba2ea43
Shahn Khan <snk.sojib@gmail.com>
2024-02-25 18:18:29 +0200

    added rechart

---
+++ src/components/barchart.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class barchart extends PureComponent {
+  render() {
+    return (
+      <div>barchart</div>
+    )
+  }
+}
+
+export default barchart
