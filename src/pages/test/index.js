f9fd46459a641ef8a2f48c6e8a4cb381ea4ea79b
Shahn Khan <snk.sojib@gmail.com>
2024-02-26 23:51:45 +0200

    update

---
+++ src/pages/test/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class index extends PureComponent {
+    render() {
+        return (
+            <div>hey text page</div>
+        )
+    }
+}
+
+export default index
