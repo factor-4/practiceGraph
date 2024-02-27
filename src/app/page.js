5f5dd5cd25ad44ea60341a929f099bb3b9a68ef2
Shahn Khan <snk.sojib@gmail.com>
2024-02-27 23:50:44 +0200

    radar chart added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 
 'use client'
 import { LineChart, Line } from 'recharts';
+import Example from '@/pages/radarchart';
 
 
 
@@ -10,17 +11,15 @@ import { LineChart, Line } from 'recharts';
 
 export default function Home() {
 
-  const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }, { name: 'Page B', uv: 450, pv: 2400, amt: 2400 }, { name: 'Page C', uv: 500, pv: 2400, amt: 2400 }, {name: 'Page D', uv: 200, pv: 2780, amt: 2550}];
+  const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }, { name: 'Page B', uv: 450, pv: 2400, amt: 2400 }, { name: 'Page C', uv: 500, pv: 2400, amt: 2400 }, { name: 'Page D', uv: 200, pv: 2780, amt: 2550 }];
 
 
 
 
 
   return (
-    <div >
-      <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      </LineChart>
+    <div style={{height:500}}>
+      <Example></Example>
     </div>
   );
 }
